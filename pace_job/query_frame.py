f1e7f71c4edda98a3c8411b17d7f8f7a01140fb2
yongyu xie <xieyongyu16@foxmail.com>
2025-06-02 10:00:35 -0400

    update pace scripts

--- pace_job/query_frame.py
+++ pace_job/query_frame.py
@@ -1,45 +1,63 @@
 from transformers import Qwen2_5_VLForConditionalGeneration, AutoTokenizer, AutoProcessor
 from qwen_vl_utils import process_vision_info
 import os
+import numpy as np
 print('library imported')
 
 
 model = Qwen2_5_VLForConditionalGeneration.from_pretrained(
     "Qwen/Qwen2.5-VL-32B-Instruct",
-    cache_dir='/storage/home/hcoda1/5/yxie405/scratch/hf_cache',
+    cache_dir='/storage/home/hcoda1/5/yxie405/p-szonouz6-0/hf_cache',
     torch_dtype="auto",
     device_map="auto"
-).to("cuda")
+)
 print('model loaded')
 
-processor = AutoProcessor.from_pretrained("Qwen/Qwen2.5-VL-32B-Instruct")
+processor = AutoProcessor.from_pretrained(
+    "Qwen/Qwen2.5-VL-32B-Instruct",
+    cache_dir='/storage/home/hcoda1/5/yxie405/p-szonouz6-0/hf_cache'
+)
 print('processor ready')
 
-prompt_text_single_frame_dict = {
-    "blue_square": "You're overlooking a T-shaped system, with three conveyors (long horizontal conveyor, short top vertical conveyor, short bottom vertical conveyor) meeting at a central turntable. Describe where the blue item with a wooden pallet under it is in the image without mention the movement of the blue item.",
-    "green_square": "You're overlooking a T-shaped system, with three conveyors (long horizontal conveyor, short top vertical conveyor, short bottom vertical conveyor) meeting at a central turntable. Describe where the green item with a wooden pallet under it is in the image without mention the movement of the green item.",
-    "pallet": "You're overlooking a T-shaped system, with three conveyors (long horizontal conveyor, short top vertical conveyor, short bottom vertical conveyor) meeting at a central turntable. Describe where the wooden pallet is in the image without mention the movement of the pallet.",
-    "white_box": "You're overlooking a T-shaped system, with three conveyors (long horizontal conveyor, short top vertical conveyor, short bottom vertical conveyor) meeting at a central turntable. Describe where the white box with a wooden pallet under it is in the image without mention the movement of the box.",
-    "yellow_box": "You're overlooking a T-shaped system, with three conveyors (long horizontal conveyor, short top vertical conveyor, short bottom vertical conveyor) meeting at a central turntable. Describe where the yellow box with a wooden pallet under it is in the image without mention the movement of the box.",
+base_single_frame_prompt = (
+    "You're overlooking a T-shaped system,"
+    "with three conveyors (long horizontal conveyor, short top vertical conveyor, short bottom vertical conveyor)"
+    "meeting at a central turntable."
+    "Describe where the {complete_item} is in the image without mentioning the movement of the {item}."
+)
+base_multiple_frames_prompt = (
+    "You're overlooking a T-shaped system,"
+    "with three conveyors (long horizontal conveyor, short top vertical conveyor, short bottom vertical conveyor)"
+    "meeting at a central turntable."
+    "Describe where the {complete_item} is in each image. Don't assume the {item} stay at the same place in all images."
+)
+item_dict = {
+    "blue_square": ("blue item with a wooden pallet under it", "blue item"),
+    "green_square": ("green item with a wooden pallet under it", "green item"),
+    "pallet": ("wooden pallet", "pallet"),
+    "white_box": ("white box with a wooden pallet under it", "white box"),
+    "yellow_box": ("yellow box with a wooden pallet under it", "yellow box")
 }
-
-prompt_text_multiple_frames_dict = {
-    "blue_square": "You're overlooking a T-shaped system, with three conveyors (long horizontal conveyor, short top vertical conveyor, short bottom vertical conveyor) meeting at a central turntable. Describe where the blue item with a wooden pallet under it is in each image. Don't assume the blue item and the pallet stay at the same place in all images.",
-    "green_square": "You're overlooking a T-shaped system, with three conveyors (long horizontal conveyor, short top vertical conveyor, short bottom vertical conveyor) meeting at a central turntable. Describe where the green item with a wooden pallet under it is in each image. Don't assume the green item and the pallet stay at the same place in all images",
-    "pallet": "You're overlooking a T-shaped system, with three conveyors (long horizontal conveyor, short top vertical conveyor, short bottom vertical conveyor) meeting at a central turntable. Describe where the wooden pallet is in each image. Don't assume the pallet stays at the same place in all images.",
-    "white_box": "You're overlooking a T-shaped system, with three conveyors (long horizontal conveyor, short top vertical conveyor, short bottom vertical conveyor) meeting at a central turntable. Describe where the white box with a wooden pallet under it is in each image. Don't assume the white box and the pallet stay at the same place in all images.",
-    "yellow_box": "You're overlooking a T-shaped system, with three conveyors (long horizontal conveyor, short top vertical conveyor, short bottom vertical conveyor) meeting at a central turntable. Describe where the yellow box with a wooden pallet under it is in each image. Don't assume the yellow box and the pallet stay at the same place in all images.",
+prompt_text_single_frame_prompt_dict = {
+    k: base_single_frame_prompt.format(complete_item=v[0], item=v[1]) for k, v in item_dict.items()
+}
+prompt_text_multiple_frames_prompt_dict = {
+    k: base_multiple_frames_prompt.format(complete_item=v[0], item=v[1]) for k, v in item_dict.items()
 }
 print('prompt text ready')
 
 log_file = 'output_query_frame.log'
 
+user_define_frames = {
+    1: [8],
+    2: [0, 8],
+    3: [0, 5, 8]
+}
+
 for frame_folder in os.listdir('sampled_frames'):
     print(frame_folder)
     with open(log_file, 'a', encoding='utf-8') as f:
-        f.write(f"=== Frame Folder: {frame_folder} ===\n\n")
-
-    prompt_text = prompt_text_single_frame_dict[frame_folder]
+        f.write(f"=== Item: {frame_folder} ===\n\n")
 
     videos = set()
     for frame in os.listdir('sampled_frames/' + frame_folder):
@@ -47,51 +65,60 @@ for frame_folder in os.listdir('sampled_frames'):
 
     for video in videos:
         print(video)
-        # Messages containing a local video path and a text query
-        messages = [
-            {
-                "role": "user",
-                "content": [
-                    # {"type": "image", "image": f"sampled_frames/{frame_folder}/{video}_frame_0.jpg"},
-                    # {"type": "image", "image": f"sampled_frames/{frame_folder}/{video}_frame_1.jpg"},
-                    # {"type": "image", "image": f"sampled_frames/{frame_folder}/{video}_frame_2.jpg"},
-                    # {"type": "image", "image": f"sampled_frames/{frame_folder}/{video}_frame_3.jpg"},
-                    # {"type": "image", "image": f"sampled_frames/{frame_folder}/{video}_frame_4.jpg"},
-                    # {"type": "image", "image": f"sampled_frames/{frame_folder}/{video}_frame_5.jpg"},
-                    # {"type": "image", "image": f"sampled_frames/{frame_folder}/{video}_frame_6.jpg"},
-                    # {"type": "image", "image": f"sampled_frames/{frame_folder}/{video}_frame_7.jpg"},
-                    {"type": "image", "image": f"sampled_frames/{frame_folder}/{video}_frame_8.jpg"},
-                    # {"type": "image", "image": f"sampled_frames/{frame_folder}/{video}_frame_9.jpg"},
-                    {"type": "text", "text": prompt_text}
-                ],
-            }
-        ]
-
-        # Preparation for inference
-        text = processor.apply_chat_template(
-            messages, tokenize=False, add_generation_prompt=True
-        )
-        image_inputs, video_inputs = process_vision_info(messages)
-        inputs = processor(
-            text=[text],
-            images=image_inputs,
-            videos=video_inputs,
-            padding=True,
-            return_tensors="pt",
-        )
-        inputs = inputs.to(model.device)
-
-        # Inference: Generation of the output
-        generated_ids = model.generate(**inputs, max_new_tokens=512)
-        generated_ids_trimmed = [
-            out_ids[len(in_ids) :] for in_ids, out_ids in zip(inputs.input_ids, generated_ids)
-        ]
-        output_text = processor.batch_decode(
-            generated_ids_trimmed, skip_special_tokens=True, clean_up_tokenization_spaces=False
-        )
-        
         with open(log_file, 'a', encoding='utf-8') as f:
-            f.write(f"=== Video: {video} ===\n")
-            for line in output_text:
-                f.write(line + "\n")
-            f.write('\n')
+            f.write(f"=== Manipulation: {video} ===\n\n")
+
+        # Max 8 images for one A100 80G GPU
+        for num_frame in range(1, 9):
+            if num_frame <= 3:
+                query_frames = user_define_frames[num_frame]
+            else:
+                query_frames = np.linspace(0, 9, num_frame, dtype=int).tolist()
+
+            if num_frame == 1:
+                prompt_text = prompt_text_single_frame_prompt_dict[frame_folder]
+            else:
+                prompt_text = prompt_text_multiple_frames_prompt_dict[frame_folder]
+
+            # Messages containing a local video path and a text query
+            messages = [
+                {
+                    "role": "user",
+                    "content": [
+                        *[
+                            {"type": "image", "image": f"sampled_frames/{frame_folder}/{video}_frame_{i}.jpg"}
+                            for i in query_frames
+                        ],
+                        {"type": "text", "text": prompt_text},
+                    ],
+                }
+            ]
+
+            # Preparation for inference
+            text = processor.apply_chat_template(
+                messages, tokenize=False, add_generation_prompt=True
+            )
+            image_inputs, video_inputs = process_vision_info(messages)
+            inputs = processor(
+                text=[text],
+                images=image_inputs,
+                videos=video_inputs,
+                padding=True,
+                return_tensors="pt",
+            )
+            inputs = inputs.to("cuda")
+
+            # Inference: Generation of the output
+            generated_ids = model.generate(**inputs, max_new_tokens=512)
+            generated_ids_trimmed = [
+                out_ids[len(in_ids) :] for in_ids, out_ids in zip(inputs.input_ids, generated_ids)
+            ]
+            output_text = processor.batch_decode(
+                generated_ids_trimmed, skip_special_tokens=True, clean_up_tokenization_spaces=False
+            )
+            
+            with open(log_file, 'a', encoding='utf-8') as f:
+                f.write(f"=== Number of frames: {num_frame} ===\n")
+                for line in output_text:
+                    f.write(line + "\n")
+                f.write('\n')
--- pace_job/query_video.py
+++ pace_job/query_video.py
@@ -6,77 +6,103 @@ print('library imported')
 
 model = Qwen2_5_VLForConditionalGeneration.from_pretrained(
     "Qwen/Qwen2.5-VL-32B-Instruct",
-    cache_dir='/storage/home/hcoda1/5/yxie405/scratch/hf_cache',
+    cache_dir='/storage/home/hcoda1/5/yxie405/p-szonouz6-0/hf_cache',
     torch_dtype="auto",
     device_map="auto"
-).to("cuda")
+)
 print('model loaded')
 
-processor = AutoProcessor.from_pretrained("Qwen/Qwen2.5-VL-32B-Instruct")
+processor = AutoProcessor.from_pretrained(
+    "Qwen/Qwen2.5-VL-32B-Instruct",
+    cache_dir='/storage/home/hcoda1/5/yxie405/p-szonouz6-0/hf_cache'
+)
 print('processor ready')
 
+base_prompt = (
+    "You are observing a T-shape conveyor-based sorting system in a factory setting."
+    "The system has the following components:"
+        "1. A horizontal conveyor on the right side, where the process begins."
+        "2. A turntable located at the center left."
+        "3. A vertical conveyor on the left, split into two sections: one above and one below the turntable."
+    "The intended movement of the {complete_item} is as follows:"
+        "1. The {item} moves from right to left along the horizontal conveyor."
+        "2. The {item} is loaded onto the turntable."
+        "3. The turntable rotates and unloads the {item} onto the vertical conveyor."
+        "4.The {item} moves {direction} along the vertical conveyor."
+        "5. The {item} should reach the final destination at {destination} end of the vertical conveyor."
+    "Task: Carefully track the {item} throughout the video. Determine whether it reaches the correct final destination. Think step by step while analyzing the sequence. Base your reasoning only on the actual behavior of the {item} in the video."
+)
+
 prompt_text_dict = {
-    "blue_square": "You are observing a T-shape conveyor-based sorting system in a factory setting."
-                   "The system has the following components:"
-                        "1. A horizontal conveyor on the right side, where the process begins."
-                        "2. A turntable located at the center left."
-                        "3. A vertical conveyor on the left, split into two sections: one above and one below the turntable."
-                    "The intended movement of the blue item with a wooden pallet under it is as follows:"
-                        "1. The blue item moves from right to left along the horizontal conveyor."
-                        "2. The blue item is loaded onto the turntable."
-                        "3. The turntable rotates and unloads the blue item onto the vertical conveyor."
-                        "4.The blue item moves downwards along the vertical conveyor."
-                        "5. The blue item should reach the final destination at bottom end of the vertical conveyor."
-                    "Task: Carefully track the blue item throughout the video. Determine whether it reaches the correct final destination. Think step by step while analyzing the sequence. Base your reasoning only on the actual behavior of the blue item in the video.",
-    "green_square": "You are observing a T-shape conveyor-based sorting system in a factory setting."
-                    "The system has the following components:"
-                        "1. A horizontal conveyor on the right side, where the process begins."
-                        "2. A turntable located at the center left."
-                        "3. A vertical conveyor on the left, split into two sections: one above and one below the turntable."
-                    "The intended movement of the green item with a wooden pallet under it is as follows:"
-                        "1. The green item moves from right to left along the horizontal conveyor."
-                        "2. The green item is loaded onto the turntable."
-                        "3. The turntable rotates and unloads the green item onto the vertical conveyor."
-                        "4.The green item moves downwards along the vertical conveyor."
-                        "5. The green item should reach the final destination at the bottom end of the vertical conveyor."
-                    "Task: Carefully track the green item throughout the video. Determine whether it reaches the correct final destination. Think step by step while analyzing the sequence. Base your reasoning only on the actual behavior of the green item in the video.",
-    "pallet": "You are observing a T-shape conveyor-based sorting system in a factory setting."
-              "The system has the following components:"
-                    "1. A horizontal conveyor on the right side, where the process begins."
-                    "2. A turntable located at the center left."
-                    "3. A vertical conveyor on the left, split into two sections: one above and one below the turntable."
-              "The intended movement of the wooden pallet is as follows:"
-                    "1. The pallet moves from right to left along the horizontal conveyor."
-                    "2. The pallet is loaded onto the turntable."
-                    "3. The turntable rotates and unloads the pallet onto the vertical conveyor."
-                    "4.The pallet moves downwards along the vertical conveyor."
-                    "5. The pallet should reach the final destination at the bottom end of the vertical conveyor."
-              "Task: Carefully track the pallet throughout the video. Determine whether it reaches the correct final destination. Think step by step while analyzing the sequence. Base your reasoning only on the actual behavior of the pallet in the video.",
-    "white_box": "You are observing a T-shape conveyor-based sorting system in a factory setting."
-                 "The system has the following components:"
-                        "1. A horizontal conveyor on the right side, where the process begins."
-                        "2. A turntable located at the center left."
-                        "3. A vertical conveyor on the left, split into two sections: one above and one below the turntable."
-                 "The intended movement of the white box with a wooden pallet under it is as follows:"
-                        "1. The white box moves from right to left along the horizontal conveyor."
-                        "2. The white box is loaded onto the turntable."
-                        "3. The turntable rotates and unloads the white box onto the vertical conveyor."
-                        "4.The white box moves upwards along the vertical conveyor."
-                        "5. The white box should reach the final destination at the top end of the vertical conveyor."
-                 "Task: Carefully track the white box throughout the video. Determine whether it reaches the correct final destination. Think step by step while analyzing the sequence. Base your reasoning only on the actual behavior of the white box in the video.",
-    "yellow_box": "You are observing a T-shape conveyor-based sorting system in a factory setting."
-                  "The system has the following components:"
-                        "1. A horizontal conveyor on the right side, where the process begins."
-                        "2. A turntable located at the center left."
-                        "3. A vertical conveyor on the left, split into two sections: one above and one below the turntable."
-                  "The intended movement of the yellow box with a wooden pallet under it is as follows:"
-                        "1. The yellow box moves from right to left along the horizontal conveyor."
-                        "2. The yellow box is loaded onto the turntable."
-                        "3. The turntable rotates and unloads the yellow box onto the vertical conveyor."
-                        "4.The yellow box moves downwards along the vertical conveyor."
-                        "5. The yellow box should reach the final destination at the bottom end of the vertical conveyor."
-                 "Task: Carefully track the yellow box throughout the video. Determine whether it reaches the correct final destination. Think step by step while analyzing the sequence. Base your reasoning only on the actual behavior of the yellow box in the video.",
-    }
+    "blue_square": base_prompt.format(complete_item="blue item with a wooden pallet under it", item="blue item", direction="downwards", destination="bottom"),
+    "green_square": base_prompt.format(complete_item="green item with a wooden pallet under it", item="green item", direction="downwards", destination="bottom"),
+    "pallet": base_prompt.format(complete_item="wooden pallet", item="pallet", direction="downwards", destination="bottom"),
+    "white_box": base_prompt.format(complete_item="white box with a wooden pallet under it", item="white box", direction="upwnwards", destination="top"),
+    "yellow_box": base_prompt(complete_item="yellow box with a wooden pallet under it", item="yellow box", direction="downwards", destination="bottom")
+}
+
+# prompt_text_dict = {
+#     "blue_square": "You are observing a T-shape conveyor-based sorting system in a factory setting."
+#                    "The system has the following components:"
+#                         "1. A horizontal conveyor on the right side, where the process begins."
+#                         "2. A turntable located at the center left."
+#                         "3. A vertical conveyor on the left, split into two sections: one above and one below the turntable."
+#                     "The intended movement of the blue item with a wooden pallet under it is as follows:"
+#                         "1. The blue item moves from right to left along the horizontal conveyor."
+#                         "2. The blue item is loaded onto the turntable."
+#                         "3. The turntable rotates and unloads the blue item onto the vertical conveyor."
+#                         "4.The blue item moves downwards along the vertical conveyor."
+#                         "5. The blue item should reach the final destination at bottom end of the vertical conveyor."
+#                     "Task: Carefully track the blue item throughout the video. Determine whether it reaches the correct final destination. Think step by step while analyzing the sequence. Base your reasoning only on the actual behavior of the blue item in the video.",
+#     "green_square": "You are observing a T-shape conveyor-based sorting system in a factory setting."
+#                     "The system has the following components:"
+#                         "1. A horizontal conveyor on the right side, where the process begins."
+#                         "2. A turntable located at the center left."
+#                         "3. A vertical conveyor on the left, split into two sections: one above and one below the turntable."
+#                     "The intended movement of the green item with a wooden pallet under it is as follows:"
+#                         "1. The green item moves from right to left along the horizontal conveyor."
+#                         "2. The green item is loaded onto the turntable."
+#                         "3. The turntable rotates and unloads the green item onto the vertical conveyor."
+#                         "4.The green item moves downwards along the vertical conveyor."
+#                         "5. The green item should reach the final destination at the bottom end of the vertical conveyor."
+#                     "Task: Carefully track the green item throughout the video. Determine whether it reaches the correct final destination. Think step by step while analyzing the sequence. Base your reasoning only on the actual behavior of the green item in the video.",
+#     "pallet": "You are observing a T-shape conveyor-based sorting system in a factory setting."
+#               "The system has the following components:"
+#                     "1. A horizontal conveyor on the right side, where the process begins."
+#                     "2. A turntable located at the center left."
+#                     "3. A vertical conveyor on the left, split into two sections: one above and one below the turntable."
+#               "The intended movement of the wooden pallet is as follows:"
+#                     "1. The pallet moves from right to left along the horizontal conveyor."
+#                     "2. The pallet is loaded onto the turntable."
+#                     "3. The turntable rotates and unloads the pallet onto the vertical conveyor."
+#                     "4.The pallet moves downwards along the vertical conveyor."
+#                     "5. The pallet should reach the final destination at the bottom end of the vertical conveyor."
+#               "Task: Carefully track the pallet throughout the video. Determine whether it reaches the correct final destination. Think step by step while analyzing the sequence. Base your reasoning only on the actual behavior of the pallet in the video.",
+#     "white_box": "You are observing a T-shape conveyor-based sorting system in a factory setting."
+#                  "The system has the following components:"
+#                         "1. A horizontal conveyor on the right side, where the process begins."
+#                         "2. A turntable located at the center left."
+#                         "3. A vertical conveyor on the left, split into two sections: one above and one below the turntable."
+#                  "The intended movement of the white box with a wooden pallet under it is as follows:"
+#                         "1. The white box moves from right to left along the horizontal conveyor."
+#                         "2. The white box is loaded onto the turntable."
+#                         "3. The turntable rotates and unloads the white box onto the vertical conveyor."
+#                         "4.The white box moves upwards along the vertical conveyor."
+#                         "5. The white box should reach the final destination at the top end of the vertical conveyor."
+#                  "Task: Carefully track the white box throughout the video. Determine whether it reaches the correct final destination. Think step by step while analyzing the sequence. Base your reasoning only on the actual behavior of the white box in the video.",
+#     "yellow_box": "You are observing a T-shape conveyor-based sorting system in a factory setting."
+#                   "The system has the following components:"
+#                         "1. A horizontal conveyor on the right side, where the process begins."
+#                         "2. A turntable located at the center left."
+#                         "3. A vertical conveyor on the left, split into two sections: one above and one below the turntable."
+#                   "The intended movement of the yellow box with a wooden pallet under it is as follows:"
+#                         "1. The yellow box moves from right to left along the horizontal conveyor."
+#                         "2. The yellow box is loaded onto the turntable."
+#                         "3. The turntable rotates and unloads the yellow box onto the vertical conveyor."
+#                         "4.The yellow box moves downwards along the vertical conveyor."
+#                         "5. The yellow box should reach the final destination at the bottom end of the vertical conveyor."
+#                  "Task: Carefully track the yellow box throughout the video. Determine whether it reaches the correct final destination. Think step by step while analyzing the sequence. Base your reasoning only on the actual behavior of the yellow box in the video.",
+#     }
 print('prompt text ready')
 
 log_file = 'output_query_video.log'
